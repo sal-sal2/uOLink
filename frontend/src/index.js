540b583e69a5da4cfc7325fc06ff45fb7ad76d56
K S <ib.salat272@gmail.com>
2026-05-30 15:25:49 -0400

    add routing and auth-based redirects

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/ReduxStore.js";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 
 ReactDOM.render(
   <Provider store = {store}>
-      <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "*" element = {<App/>} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
