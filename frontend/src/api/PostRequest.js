e4868eab41fd04072d5536bdf852873d0a8de6b2
K S <ib.salat272@gmail.com>
2026-09-06 20:39:30 -0400

    update app url

--- frontend/src/api/PostRequest.js
+++ frontend/src/api/PostRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'https://uolink.ib-salat272.workers.dev' });
 
 export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`)
 export const likePost = (id, userId) => API.put(`post/${id}/like`, {userId: userId})
--- frontend/src/api/UploadRequest.js
+++ frontend/src/api/UploadRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'https://uolink.ib-salat272.workers.dev' });
 
 export const uploadImage = (data) => API.post('/upload/', data) 
 export const uploadPost = (data) => API.post('post', data)
